aa74ac8a3dcdcfdd941ca1830994beeeaf8adcb0
Gerard Talatinian <gerard.talatinian@gmail.com>
2026-04-27 12:22:16 -0400

    Document why fidget.nvim was added to replace Mason's progress UI

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,1 +1,4 @@
+-- Fidget shows LSP progress messages (startup spinners, indexing status) in a
+-- corner overlay. Added to replace the visual feedback Mason previously
+-- provided when LSP servers were installing and initializing.
 require('fidget').setup({})
