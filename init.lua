4524b3fa93d86e0395f0378e1a8fa49f75797766
Gerard Talatinian <gerard.talatinian@gmail.com>
2026-04-27 11:15:38 -0400

    Restore and update comments in init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- --------------------------------------------------------
+-- Setup options (prior to plugins load/config).
+-- ---------------------------------------------------------
 require('config.options')
+
+-- ---------------------------------------------------------
+-- Setup key mappings (prior to plugins load/config).
+-- ---------------------------------------------------------
 require('config.keymappings')
+
+-- ---------------------------------------------------------
+-- Plugin installation and setup via vim.pack (Neovim 0.12+)
+-- ---------------------------------------------------------
 require('config.packages')
